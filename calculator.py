af6f21383980641b76b8c455a6829cd2b9774289
nikbush <kolia.kun@gmail.com>
2024-08-03 17:31:30 +0300

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,35 @@
+import sys
+
+symbol, x, y = sys.argv[1], sys.argv[2], sys.argv[3]
+
+if (not x.isdigit()) or (not y.isdigit()):
+    print('error')
+    sys.exit()
+else:
+    x, y = int(x), int(y)
+
+
+def calc(symbol, x, y):
+    if symbol == '+':
+        return print(x + y)
+    elif symbol == '-':
+        return print(x - y)
+    elif symbol == '*':
+        return print(x * y)
+    elif symbol == '/':
+        if y == 0:
+            return print("error")
+        return print(x / y)
+    elif symbol == '//':
+        if y == 0:
+            return print("error")
+        return print(x // y)
+    elif symbol == '%':
+        if y == 0:
+            return print("error")
+        return print(x % y)
+    else:
+        return print("error")
+
+
+calc(symbol, x, y)
